80a49c7cf89947b359f715f5d67a5ec870b619d1
Levi D <devries@usna.edu>
2021-04-20 12:32:23 -0400

    updated hough transform approach

--- src/lane_detection_realsense_simplified.py
+++ src/lane_detection_realsense_simplified.py
@@ -13,7 +13,7 @@ import PIL
 
 NODE_NAME = "lane_detection_realsense_node"
 SUB_TOPIC_color = "camera/color/image_raw"
-SUB_TOPIC_depth = "camer/aligned_depth_to_color/image_raw"
+SUB_TOPIC_depth = "camera/aligned_depth_to_color/image_raw"
 PUB_TOPIC = "debug_image"
 PUB_SETPOINT_TOPIC = "lane_curvature"
 PUB_STATE_TOPIC = "lane_offset"
@@ -55,10 +55,12 @@ class LaneDetectionNode:
         self.threshold_high = rospy.get_param("/autonomous_driving/lane_detection_node/threshold_high", 150)
         self.aperture = rospy.get_param("/autonomous_driving/lane_detection_node/aperture", 3)
 
+        self.cv_image_col = []
+        self.cv_image_depth = []
         self.track_lane_width = 1.067 # meters, width of a lane on the track
         self.look_ahead_dist = 0.75 # meters, distance ahead from which lane is measured
-
-        #self.Kmat = np.linalg.inv(([304.5852355957031,0.0,213.36288452148438],[0.0,304.6020202636719,120.16206359863281],[0.0,0.0,1.0]))
+        self.K = np.array([[304.5852355957031, 0.0, 213.36288452148438],[0.0, 304.6020202636719, 120.16206359863281],[0.0, 0.0, 1.0]])
+        self.Kinv = np.linalg.inv(self.K)
         rospy.spin()
 
 
@@ -68,9 +70,9 @@ class LaneDetectionNode:
         except CvBridgeError as e:
             rospy.logerr(e)
 
-    def depthCallback(self):
+    def depthCallback(self,depth_image):
         try:
-            self.cv_image_depth = self.bridge.imgmsg_to_cv2(color_image, "bgr8")
+            self.cv_image_depth = self.bridge.imgmsg_to_cv2(depth_image, "16UC1")
         except CvBridgeError as e:
             rospy.logerr(e)
 
@@ -81,105 +83,62 @@ class LaneDetectionNode:
         h, s, gray = cv2.split(hsvd)
 
         # blur
-        blurred = cv2.GaussianBlur(gray, (self.deviation, self.deviation), self.border)
+        #blurred = cv2.GaussianBlur(gray, (self.deviation, self.deviation), self.border)
         #blurred = self.img_prep.blur(gray, , self.border)
 
         # canny
-        canny = cv2.Canny(blurred, self.threshold_low, self.threshold_high, self.aperture)
+        canny = cv2.Canny(gray, self.threshold_low, self.threshold_high, self.aperture)
+
+        # map into 3D coordinates
+        #print(self.cv_image_depth[3][8])
+        #u, v = np.mgrid[:gray.shape[1], :gray.shape[0]]
+        #x_coords = self.cv_image_depth / self.K[0][0] * (self.K[0][2] - u)
+        #y_coords = self.cv_image_depth / self.K[1][1] * (self.K[1][2] - v)
+
 
         # crop image
         above = 0.6
-        below = 0.3
+        below = 0.25
         side = 0.0
         height, width = canny.shape
         cropped = canny[int((height*above)):height - int((height*below)), int((width*side)):width - int((width*side))]
-
-
-        nwindows=1 # number of windows to divide frame into
-        margin=65 # horizontal margin that lane must fall within
-        minpix = 1 # minimum number of pixels within a window
-        left_fit_= np.empty(3)
-        right_fit_ = np.empty(3)
-        # histogram = np.sum(canny[canny.shape[0]//2:,:], axis=0)
-        histogram = np.sum(cropped, axis=0)
-
-        # find peaks of left and right halves
-        midpoint = int(histogram.shape[0]/2)
-        leftx_base = np.argmax(histogram[:midpoint])
-        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
-
-        # Set height of windows
-        window_height = np.int(cropped.shape[0]/nwindows)
-
-        # Identify the x and y positions of all nonzero pixels in the image
-        nonzero = cropped.nonzero()
-        nonzeroy = np.array(nonzero[0])
-        nonzerox = np.array(nonzero[1])
-
-
-        # Current positions to be updated for each window
-        leftx_current = leftx_base
-        rightx_current = rightx_base
-
-        # Create empty lists to receive left and right lane pixel indices
-        left_lane_inds = []
-        right_lane_inds = []
-
-        left_a, left_b, left_c = [],[],[]
-        right_a, right_b, right_c = [],[],[]
-
+        #
+        # # histogram = np.sum(canny[canny.shape[0]//2:,:], axis=0)
+        # histogram = np.sum(cropped, axis=0)
+        #
+        # # find peaks of left and right halves
+        # midpoint = int(histogram.shape[0]/2)
+        # leftx_base = np.argmax(histogram[:midpoint])
+        # rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+        #
+        #
         # Lane Detection
         canny = cv2.cvtColor(cropped, cv2.COLOR_GRAY2BGR)
 
+        lines = cv2.HoughLines(cropped,1,np.pi/180,35)
+        slope = []
+        if lines is not None:
+            print(len(lines))
+            for i in range(0,len(lines)):
+                rho = lines[i][0][0]
+                print(rho)
+                theta = lines[i][0][1]
+                a = np.cos(theta)
+                b = np.sin(theta)
+                x0 = a*rho
+                y0 = b*rho
+                x1 = int(x0 + 1000*(-b))
+                y1 = int(y0 + 1000*(a))
+                x2 = int(x0 - 1000*(-b))
+                y2 = int(y0 - 1000*(a))
+                slope.append((y2-y1)/(x2-x1))
+                if slope[i]<0.39 and slope[i]>0.24:
+                    cv2.line(canny,(x1,y1),(x2,y2),(255,0,255),2)
+                if slope[i]<-0.24 and slope[i]>-0.39:
+                    cv2.line(canny,(x1,y1),(x2,y2),(0,255,255),2)
+        #print(slope)
+        # print(min(slope))
 
-        # Step through the windows one by one
-        for window in range(nwindows):
-            # Identify window boundaries in x and y (and right and left)
-            win_y_low = canny.shape[0] - (window+1)*window_height
-            win_y_high = canny.shape[0] - window*window_height
-            win_xleft_low = leftx_current - margin
-            win_xleft_high = leftx_current + margin
-            win_xright_low = rightx_current - margin
-            win_xright_high = rightx_current + margin
-            # Draw the windows on the visualization image
-            if True:
-                cv2.rectangle(canny,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
-                (100,255,255), 3)
-                cv2.rectangle(canny,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
-                (255,0,0), 3)
-
-            # Identify the nonzero pixels in x and y within the window
-            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
-            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
-            # Append these indices to the lists
-            left_lane_inds.append(good_left_inds)
-            right_lane_inds.append(good_right_inds)
-            # If you found > minpix pixels, recenter next window on their mean position
-            if len(good_left_inds) > minpix:
-                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
-            if len(good_right_inds) > minpix:
-                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
-
-
-        # Concatenate the arrays of indices
-        left_lane_inds = np.concatenate(left_lane_inds)
-        right_lane_inds = np.concatenate(right_lane_inds)
-
-        # Extract left and right line pixel positions
-        leftx = nonzerox[left_lane_inds]
-        lefty = nonzeroy[left_lane_inds]
-        rightx = nonzerox[right_lane_inds]
-        righty = nonzeroy[right_lane_inds]
-
-        # Project pixel points into 3d space
-        
-
-        #
-        # # Fit a second order polynomial to each lane
-        # left_fit = np.polyfit(lefty, leftx, 2)
-        # right_fit = np.polyfit(righty, rightx, 2)
 
         # calculate lane_curvature
         left_curv = 0.3
